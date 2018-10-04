f41c02d9d5903197a0310b1dff60b87c258d8966
Jack Lam <jlam00@stuy.edu>
2018-10-03 22:54:52 -0400

    Made BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public BankAccount( int accountID, double balance, String password){...}
+
+public String toString(){...}
+
+public double getBalance(){...}
+
+public int getAccountID(){...}
+
+
+ public void setPassword(String newPass){...}
+
+
+ public boolean deposit(double amount){...}
+
+ public boolean withdraw(double amount){... }
